f703d78218e42520d68c4d76639edc65c1782ea9
Karina Chen <karina6188@gmail.com>
2019-10-15 14:23:24 -0700

    Main and StartSequence methods

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # Lab01-Exception-Handling
+
+About Me
+Lab01-Exception-Handling
+
+Author: Karina Chen
+
+Description
+This is a C# console application that will ask a user to input numbers. The numbers will be taken in to set up an array based on the input value. The end result of this application is to output the values of the sum, the product, and the quotient of the array.
+
+Getting Started
+Clone this repository to your local machine.
+
+$ git clone [https://github.com/karina6188/Lab01-Exception-Handling.git]
+To run the program from Visual Studio:
+Select File -> Open -> Project/Solution
+
+Next navigate to the location you cloned the Repository.
+
+Double click on the Lab01-Execption-Handling directory.
+
+Then select and open lab01-exception-handling.sln
+
+Visuals
+![Alt execution capture]()
+
+Application Start
+Image 1
+
+Using the Application
+Image 1
+
+Application End
+Image 1
+
+Change Log
+1.1: Created the project and added Main, StartSequence methods - 2019 Oct 15
---
+++ lab01-exception-handling/lab01-exception-handling/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace lab01_exception_handling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try 
+            {
+                 StartSequence();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Something went wrong");
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Program is complete.");
+                Console.ReadLine();
+            }
+        }
+
+        static void StartSequence()
+        {
+            Console.WriteLine("Welcom to my game! Let's do some math!");
+            Console.WriteLine("please enter a number greater than zero.");
+            try
+            {
+                int number = Convert.ToInt32(Console.ReadLine());
+                int[] array = new int[number];
+                Populate(array);
+                int sum = GetSum(array);
+                int product = GetProduct(array, sum);
+                int quotient = GetQuotient(product);
+                Console.WriteLine($"Your array is size: {number}");
+                Console.WriteLine($"The numbers in the array are {string.Join(",", array)}");
+                Console.WriteLine($"The sum of the array is {sum}");
+                Console.WriteLine($"{sum} * {array[number - 1]} = {product}");
+                Console.WriteLine($"{product} / {quotient} = {product / quotient}");
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (OverflowException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        static int[] Populate(int[] arr)
+        {
+
+        }
+
+        static int GetSum(int[] arr)
+        {
+
+        }
+
+        static int GetProduct(int[] arr, int sum)
+        {
+
+        }
+
+        static int GetQuotient(int product)
+        {
+
+        }
+
+    }
+}
